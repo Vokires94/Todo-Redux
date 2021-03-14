bc1da0bc1bf14ca20b7af3feea89f5d0ac38cadd
Pavlo Sierikov <Pavlo.Sierikov@ecentria.com>
2021-03-14 18:57:13 +0200

    Add OnEnter handler for AddToDo Button

--- src/Components/AddTodo.js
+++ src/Components/AddTodo.js
@@ -6,6 +6,12 @@ import { addToDo, cleanToDo, cleanDoneNotes } from '../Actions';
 function AddTodo() {
     const [listdata, setList] = useState('');
 
+    function onEnterKeyPress(e){
+      if (e.charCode === 13) {
+        addlistvalue();
+      }
+    }
+
     const addlistvalue = () => {
       if(listdata!=="") {
         store.dispatch(addToDo(listdata));
@@ -25,6 +31,7 @@ function AddTodo() {
       <div>
         <input
           onChange={(e) => setList(e.target.value)}
+          onKeyPress={(e) => onEnterKeyPress(e)}
           value={listdata}
         />
         <button className="add-todo" onClick={addlistvalue}>
